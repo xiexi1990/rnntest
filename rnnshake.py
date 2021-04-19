791ab59337354b88a728297f14b96fe17242c4c6
xiexi <xiexi_1990@126.com>
2021-04-19 17:23:14 +0800

    add flatten after gru

--- rnnshake.py
+++ rnnshake.py
@@ -11,24 +11,25 @@ tf.compat.v1.keras.backend.set_session(sess)
 path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
 text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+text = text[0:len(text)//20]
 vocab = sorted(set(text))
 print ('{} unique characters'.format(len(vocab)))
 char2idx = {u:i for i, u in enumerate(vocab)}
 idx2char = np.array(vocab)
 text_as_int = np.array([char2idx[c] for c in text])
 
-seq_length = 100
+seq_length = 5
 examples_per_epoch = len(text)//seq_length
 
 char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 
-sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
 #for item in sequences.take(5):
 #  print(repr(''.join(idx2char[item.numpy()])))
 
 def split_input_target(chunk):
     input_text = chunk[:-1]
-    target_text = chunk[1:]
+    target_text = chunk[5]
     return input_text, target_text
 
 dataset = sequences.map(split_input_target)
@@ -44,15 +45,14 @@ dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
 #vocab_size = len(vocab)
 
-seq_length = 100
-vocab_size = 65
 embedding_dim = 128
-rnn_units = 256
+rnn_units = 192
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
   model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, seq_length]),
  #   tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
     tf.keras.layers.GRU(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(vocab_size)
   ])
   return model
@@ -87,12 +87,12 @@ if True:
 
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="tblog")
 
-    history = model.fit(dataset, epochs=1, callbacks=[checkpoint_callback, tensorboard_callback])
+    history = model.fit(dataset, epochs=10, callbacks=[checkpoint_callback, tensorboard_callback])
 else:
 
-    model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+    model = build_model(len(vocab), embedding_dim, rnn_units, batch_size=1)
     model.load_weights(tf.train.latest_checkpoint('./training_checkpoints'))
-    model.build(tf.TensorShape([1, None]))
+    model.build(tf.TensorShape([1, seq_length]))
     model.summary()
 
 
@@ -100,7 +100,7 @@ def generate_text(model, start_string):
     # 评估步骤（用学习过的模型生成文本）
 
     # 要生成的字符个数
-    num_generate = 1000
+    num_generate = 200
 
     # 将起始字符串转换为数字（向量化）
     input_eval = [char2idx[s] for s in start_string]
@@ -119,18 +119,20 @@ def generate_text(model, start_string):
     for i in range(num_generate):
         predictions = model(input_eval)
         # 删除批次的维度
-        predictions = tf.squeeze(predictions, 0)
+   #     predictions = tf.squeeze(predictions, 0)
 
         # 用分类分布预测模型返回的字符
         predictions = predictions / temperature
-        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[0].numpy()
 
         # 把预测字符和前面的隐藏状态一起传递给模型作为下一个输入
-        input_eval = tf.expand_dims([predicted_id], 0)
+        input_eval2 = tf.cast(tf.expand_dims(predicted_id, 0), tf.int32)
 
-        text_generated.append(idx2char[predicted_id])
+        input_eval3 = tf.cast(predicted_id, tf.int32)
+        input_eval = tf.expand_dims(tf.concat([input_eval[0][1:5], input_eval3], 0),0)
+        text_generated.append(idx2char[predicted_id[0]])
 
     return (start_string + ''.join(text_generated))
 
-
-print(generate_text(model, start_string=u"ROMEO: "))
+a = generate_text(model, start_string=u"ROMEO")
+print(a)
