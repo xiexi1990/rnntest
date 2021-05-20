d9a69b828b003dd35c1907ee6652e56cf45b4d95
xiexi <xiexi_1990@126.com>
2021-05-20 16:30:17 +0800

    a correction to lstm_use_for

--- lstm_use_for.py
+++ lstm_use_for.py
@@ -1,41 +1,35 @@
 import tensorflow as tf
 import numpy as np
-import sys
-
 batch_size = 1
 hidden_size = 4
 num_steps = 3
 input_dim = 5
-
 np.random.seed(123)
-#input = np.random.randn(batch_size, num_steps, input_dim)
 input = np.ones([batch_size, num_steps, input_dim], dtype=int)
-
-#input[1, 6:] = 0
 x = tf.placeholder(dtype=tf.float32, shape=[batch_size, num_steps, input_dim], name='input_x')
 lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=hidden_size)
-initial_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
-
+z_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
 outputs = []
-with tf.variable_scope('RNN', initializer= tf.ones_initializer):
+
+with tf.variable_scope('for_loop', initializer= tf.ones_initializer):
+    #out, stat = tf.nn.dynamic_rnn(lstm_cell, x, initial_state=z_state)
+    #out2,stat2 = tf.nn.dynamic_rnn(lstm_cell,x[:,1,:], initial_state=stat)
+
+ #   output = lstm_cell(x[:, 0, :], z_state)
+
+ #   output2 = lstm_cell(x[:, 1, :], output[1])
+
     for i in range(num_steps):
-      #  if i > 0:
-            # print(tf.get_variable_scope())
-      #      tf.get_variable_scope().reuse_variables()
 
-        output = lstm_cell(x[:, i, :], initial_state)
-
-        outputs.append(output)
         if i > 0:
-            outputs[i]
-
+         #   tf.get_variable_scope().reuse_variables()
+            output = lstm_cell(x[:, i, :], outputs[i-1][1])
+        else:
+            output = lstm_cell(x[:, i, :], z_state)
+        outputs.append(output)
 with tf.Session() as sess:
     init_op = tf.initialize_all_variables()
-
     sess.run(init_op)
-
-    np.set_printoptions(threshold=sys.maxsize)
-
     result = sess.run(outputs, feed_dict={x: input})
-    print(np.asarray(result))
+    print(result)
 
