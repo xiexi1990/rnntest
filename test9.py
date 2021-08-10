a4f13b9f1a2005dee9e59a1091ca10ec594f09a1
xiexi <xiexi_1990@126.com>
2021-08-10 18:09:37 +0800

    lb

--- test9.py
+++ test9.py
@@ -1,14 +1,55 @@
-import tensorflow as tf
+import cv2 as  cv
 import numpy as np
 
-def gen_series():
-    i = 0
-    while True:
-        size = np.random.randint(0, 10)
-        yield i, np.random.normal(size=(size,))
-        i += 1
+width = 5
+height = 12
+qipan_cell = 220
 
-ds_series = tf.data.Dataset.from_generator(
-    gen_series,
-    output_types=(tf.int32, tf.float32),
-    output_shapes=((), (None,)))
+width_pix = (width + 1) * qipan_cell   # add extra  qipan_cell  for reserve blank
+height_pix = (height + 1) * qipan_cell
+
+
+
+# white = (255,255,255)
+# black =  (0,0,0)
+
+image = np.zeros((height_pix, width_pix, 3), dtype=np.uint8)
+image.fill(255)
+
+# 创建显示窗口
+win_name = "qipan"
+cv.namedWindow("qipan", cv.WINDOW_AUTOSIZE)
+cv.imshow(win_name, image)
+color = (255, 255, 255)
+
+y0 = 0
+fill_color = 0
+for j in range(0, height + 1):
+    y = j * qipan_cell
+    for i in range(0, width + 1):
+        # rint(i)
+        x0 = i * qipan_cell
+        y0 = y
+        rect_start = (x0, y0)
+
+        x1 = x0 + qipan_cell
+        y1 = y0 + qipan_cell
+        rect_end = (x1, y1)
+        print(x0, y0, x1, y1, fill_color)
+        cv.rectangle(image, rect_start, rect_end, color, 1, 0)
+        # print(fill_color)
+        image[y0:y1, x0:x1] = fill_color
+        if width % 2:
+            if i != width:
+                fill_color = (0 if (fill_color == 255) else 255)
+        else:
+            if i != width + 1:
+                fill_color = (0 if (fill_color == 255) else 255)
+
+# image[0:20,0:20] = 0
+# image[40:60,0:20] = 0
+
+cv.imwrite("qipan_%d_W_%d_H.jpg" % (width, height), image)
+cv.imshow(win_name, image)
+
+cv.waitKey()
