772c01ed9455cef44cf1331f9aaa25a898f8e9e7
xiexi1990 <xiexi_1990@126.com>
2021-10-07 16:24:35 +0800

    train and draw success

--- rnn_shake2.py
+++ rnn_shake2.py
@@ -46,21 +46,59 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
     tf.keras.layers.Dense(vocab_size)
   ])
   return model
-model = build_model(
-  vocab_size = len(vocab),
-  embedding_dim=embedding_dim,
-  rnn_units=rnn_units,
-  batch_size=BATCH_SIZE)
-def loss(labels, logits):
-  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
-model.compile(optimizer='adam', loss=loss)
 
-# 检查点保存至的目录
 checkpoint_dir = './training_checkpoints'
-# 检查点的文件名
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
-checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_prefix,
-    save_weights_only=True)
-EPOCHS=10
-history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+if False:
+    model = build_model(
+      vocab_size = len(vocab),
+      embedding_dim=embedding_dim,
+      rnn_units=rnn_units,
+      batch_size=BATCH_SIZE)
+    def loss(labels, logits):
+      return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+    model.compile(optimizer='adam', loss=loss)
+
+    # 检查点保存至的目录
+
+    # 检查点的文件名
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+    checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_prefix,
+        save_weights_only=True)
+    EPOCHS=10
+    history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+else:
+    model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+    model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+    model.build(tf.TensorShape([1, None]))
+
+def generate_text(model, start_string):
+  num_generate = 1000
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+  text_generated = []
+  # 低温度会生成更可预测的文本
+  # 较高温度会生成更令人惊讶的文本
+  temperature = 1.0
+  # 这里批大小为 1
+  hid = model.reset_states()
+
+  print(model(np.array([[2]])))
+  print(model(np.array([[2]])))
+  print(model(np.array([[2]])))
+
+
+
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # 删除批次的维度
+      predictions = tf.squeeze(predictions, 0)
+      # 用分类分布预测模型返回的字符
+      predictions = predictions / temperature
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+      input_eval = tf.expand_dims([predicted_id], 0)
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+print(generate_text(model, start_string=u"ROMEO: "))
--- test16.py
+++ test16.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.python.keras.layers.recurrent import DropoutRNNCellMixin, AbstractRNNCell
 from tensorflow.python.util import nest
+import matplotlib.pyplot as plt
 
 tf.random.set_seed(123)
 np.random.seed(1234)
@@ -21,10 +22,6 @@ config.gpu_options.allow_growth=True
 sess = tf.compat.v1.Session(config=config)
 tf.compat.v1.keras.backend.set_session(sess)
 
-units = 16
-in_tan_dim = 10
-M = 8
-
 class SGRUCell(DropoutRNNCellMixin, keras.layers.Layer):
     def __init__(self, units, in_tanh_dim, nclass, dropout=0., recurrent_dropout=0., **kwargs):
         super(SGRUCell, self).__init__(**kwargs)
@@ -33,31 +30,25 @@ class SGRUCell(DropoutRNNCellMixin, keras.layers.Layer):
         self.nclass = nclass
         self.dropout = dropout
         self.recurrent_dropout = recurrent_dropout
-
     @property
     def state_size(self):
         return self.units
     @property
     def output_size(self):
         return self.units
-
     def build(self, input_shape):
         input_dim = input_shape[-1]
         assert len(input_shape) == 2 and input_dim == 6
-
-        self.Wd = self.add_weight(shape=(2, self.in_tanh_dim), initializer='glorot_uniform')
-        self.bd = self.add_weight(shape=(self.in_tanh_dim), initializer='zeros')
-        self.Ws = self.add_weight(shape=(3, self.in_tanh_dim), initializer='glorot_uniform')
-        self.bs = self.add_weight(shape=(self.in_tanh_dim), initializer='zeros')
-
-        self.recurrent_kernel = self.add_weight(shape=(self.units, self.units * 4), initializer='orthogonal')
-        self.kernel_d = self.add_weight(shape=(self.in_tanh_dim, self.units*4), initializer='glorot_uniform')
-        self.kernel_s = self.add_weight(shape=(self.in_tanh_dim, self.units*4), initializer='glorot_uniform')
-        self.kernel_c = self.add_weight(shape=(self.nclass, self.units * 4), initializer='glorot_uniform')
-        self.bias = self.add_weight(shape=(self.units*4), initializer='zeros')
-
+        self.Wd = self.add_weight(shape=(2, self.in_tanh_dim), initializer='glorot_uniform', name='Wd')
+        self.bd = self.add_weight(shape=(self.in_tanh_dim), initializer='zeros', name='bd')
+        self.Ws = self.add_weight(shape=(3, self.in_tanh_dim), initializer='glorot_uniform', name='Ws')
+        self.bs = self.add_weight(shape=(self.in_tanh_dim), initializer='zeros', name='bs')
+        self.recurrent_kernel = self.add_weight(shape=(self.units, self.units * 4), initializer='orthogonal', name='recurrent_kernel')
+        self.kernel_d = self.add_weight(shape=(self.in_tanh_dim, self.units*4), initializer='glorot_uniform', name='kernel_d')
+        self.kernel_s = self.add_weight(shape=(self.in_tanh_dim, self.units*4), initializer='glorot_uniform', name='kernel_s')
+        self.kernel_c = self.add_weight(shape=(self.nclass, self.units * 4), initializer='glorot_uniform', name='kernel_c')
+        self.bias = self.add_weight(shape=(self.units*4), initializer='zeros', name='bias')
         self.built = True
-
     def call(self, inputs, states, training):
         h_tm1 = states[0] if nest.is_sequence(states) else states  # previous memory
         d = inputs[:, 0:2]
@@ -114,8 +105,6 @@ class SGRUCell(DropoutRNNCellMixin, keras.layers.Layer):
                        + self.bias[self.units * 3:])
         new_state = [h] if nest.is_sequence(states) else h
         return o, new_state
-
-
     def get_config(self):
         config = super(SGRUCell, self).get_config()
         config.update({'units': self.units, 'in_tanh_dim': self.in_tanh_dim, 'nclass': self.nclass, 'dropout': self.dropout, 'recurrent_dropout': self.recurrent_dropout})
@@ -125,16 +114,13 @@ class PostProcess(keras.layers.Layer):
     def __init__(self, M, **kwargs):
         super(PostProcess, self).__init__(**kwargs)
         self.M = M
-
     def build(self, input_shape):
         input_dim = input_shape[-1]
-        self.Wgmm = self.add_weight(shape=(input_dim, self.M * 5), initializer='glorot_uniform')
-        self.bgmm = self.add_weight(shape=(self.M * 5), initializer='zeros')
-        self.Wsoftmax = self.add_weight(shape=(input_dim, 3), initializer='glorot_uniform')
-        self.bsoftmax = self.add_weight(shape=(3), initializer='zeros')
+        self.Wgmm = self.add_weight(shape=(input_dim, self.M * 5), initializer='glorot_uniform', name='Wgmm')
+        self.bgmm = self.add_weight(shape=(self.M * 5), initializer='zeros', name='bgmm')
+        self.Wsoftmax = self.add_weight(shape=(input_dim, 3), initializer='glorot_uniform', name='Wsoftmax')
+        self.bsoftmax = self.add_weight(shape=(3), initializer='zeros', name='bsoftmax')
         self.built = True
-
-
     def call(self, inputs, **kwargs):
         R5M = tf.matmul(inputs, self.Wgmm) + self.bgmm
         _pi = R5M[:, :, :self.M]
@@ -143,12 +129,9 @@ class PostProcess(keras.layers.Layer):
         muy = R5M[:, :, self.M * 2:self.M * 3]
         sigmax = tf.exp(R5M[:, :, self.M * 3:self.M * 4])
         sigmay = tf.exp(R5M[:, :, self.M * 4:])
-
         R3 = tf.matmul(inputs, self.Wsoftmax) + self.bsoftmax
         p = tf.exp(R3) / tf.reduce_sum(tf.exp(R3), axis=-1, keepdims=True)
-
-        return zip(pi, mux, muy, sigmax, sigmay, p)
-
+        return tf.concat([pi, mux, muy, sigmax, sigmay, p], axis=-1)
     def get_config(self):
         config = super(PostProcess, self).get_config()
         config.update({"M": self.M})
@@ -160,36 +143,13 @@ def N(x, mu, sigma):
 loss_tracker = keras.metrics.Mean(name="loss")
 mae_metric = keras.metrics.MeanAbsoluteError(name="mae")
 
-rnn_cell = SGRUCell(units=units, in_tanh_dim=in_tan_dim, nclass=nclass, dropout=0., recurrent_dropout=0.)
-rnn_layer = tf.keras.layers.RNN(rnn_cell, return_state=False, return_sequences=True)
-post_process_layer = PostProcess(M=M)
-#
-# print(tf.executing_eagerly())
-#
+units = 16
+in_tanh_dim = 10
+M = 8
 
-#
-# a = take_batches.as_numpy_iterator().__next__()
-# d = rnn_layer(a[0])
-# loss = keras.losses.mean_squared_error(a[1], tf.matmul(d, tf.ones((16, 6))))
-# pi, mux, muy, sigmax, sigmay, p = post_process_layer(d)
-#
-# y = a[1]
-# xtp1 = tf.expand_dims(y[:,:,0], axis=-1)
-# ytp1 = tf.expand_dims(y[:,:,1], axis=-1)
-# stp1 = y[:,:,2:5]
-#
-# w = tf.constant([1,5,100], dtype=tf.float32)
-#
-# lPd = tf.math.log(tf.reduce_sum(pi * N(xtp1, mux, sigmax) * N(ytp1, muy, sigmay), axis=-1))
-# lPs= tf.reduce_sum(w * stp1 * tf.math.log(p), axis=-1)
-#
-# loss = - tf.reduce_sum(lPd + lPs, axis=-1)
-#
-#
-#exit()
-
-def loss(x, y):
-    pi, mux, muy, sigmax, sigmay, p = *x
+def loss(y, pred):
+    pi, mux, muy, sigmax, sigmay = tf.split(pred[:, :, :M * 5], 5, axis=-1)
+    p = pred[:, :, M * 5:]
     xtp1 = tf.expand_dims(y[:, :, 0], axis=-1)
     ytp1 = tf.expand_dims(y[:, :, 1], axis=-1)
     stp1 = y[:, :, 2:5]
@@ -198,59 +158,79 @@ def loss(x, y):
     lPs = tf.reduce_sum(w * stp1 * tf.math.log(p), axis=-1)
     return - (lPd + lPs)
 
-model = keras.Sequential([
-    keras.layers.Input(shape=(None, 6), dtype=tf.float32, ragged=False),
-    rnn_layer,
-    post_process_layer
-])
-model.compile(optimizer="adam", loss=loss)
-model.fit(take_batches, steps_per_epoch=100, epochs=100)
+def construct_model(rnn_cell_units, in_tanh_dim, nclass, stateful, M, batch_shape):
+    rnn_cell = SGRUCell(units=rnn_cell_units, in_tanh_dim=in_tanh_dim, nclass=nclass, dropout=0., recurrent_dropout=0.)
+    rnn_layer = tf.keras.layers.RNN(rnn_cell, return_state=False, return_sequences=True, stateful=stateful)
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(batch_shape=batch_shape),
+        rnn_layer,
+        PostProcess(M=M)
+      ])
+    return model
+
+def draw_chars(model, classes, maxlen):
+    fig = plt.figure()
+    ch_cnt = 1
+    for ch in classes:
+        ch_plt = fig.add_subplot(1, len(classes), ch_cnt)
+        model.reset_states()
+        pnt_in = np.array([[[0, 0, 0, 0, 0, ch]]])
+        pnt_cnt = 0
+        cur_x = 0
+        cur_y = 0
+
+        while pnt_cnt < maxlen:
+            pred = np.squeeze(model(pnt_in))
+            pi, mux, muy, sigmax, sigmay = np.split(pred[:M * 5], 5, axis=-1)
+            p = pred[M * 5:]
+            r1 = np.random.rand()
+            sum = 0
+            for i in range(M):
+                sum += pi[i]
+                if sum > r1:
+                    [x_pred, y_pred] = np.random.multivariate_normal([mux[i], muy[i]], [[np.square(sigmax[i]), 0], [0, np.square(sigmay[i])]])
+                    break
+            r2 = np.random.rand()
+            sum = 0
+            s_pred = np.zeros(3)
+            for i in range(3):
+                sum += p[i]
+                if sum > r2:
+                    s_pred[i] = 1
+                    break
+
+            next_x = cur_x + x_pred
+            next_y = cur_y + y_pred
+            if(s_pred[2] == 1):
+                break
+            if(s_pred[0] == 1):
+                ch_plt.plot([cur_x, next_x], [cur_y, next_y], color='black')
+            cur_x = next_x
+            cur_y = next_y
+            pnt_in[0, 0, 0] = x_pred
+            pnt_in[0, 0, 1] = y_pred
+            pnt_in[0, 0, 2:5] = s_pred
+            pnt_cnt += 1
+        ch_cnt += 1
+    plt.show()
+
+ckdir = 'gru2'
+if True:
+    model = construct_model(units, in_tanh_dim, nclass, False, M, [None, None, 6])
+    model.compile(loss=loss, optimizer=keras.optimizers.Adam())
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=ckdir + '/ck_{epoch}', save_weights_only=True)
+    model.fit(take_batches, steps_per_epoch=30, epochs=10, callbacks=[checkpoint_callback])
+
+else:
+    model = construct_model(units, in_tanh_dim, nclass, True, M, [1, 1, 6])
+    model.load_weights(tf.train.latest_checkpoint(ckdir))
+    model.build(tf.TensorShape([1, 1, 6]))
+    draw_chars(model, [0, 1, 2], 100)
+
+exit()
 
 
 
-# class CustomModel(keras.Model):
-#     def train_step(self, data):
-#         x, y = data
-#         with tf.GradientTape() as tape:
-#             pi, mux, muy, sigmax, sigmay, p = self(x, training=True)  # Forward pass
-#             xtp1 = tf.expand_dims(y[:,:,0], axis=-1)
-#             ytp1 = tf.expand_dims(y[:,:,1], axis=-1)
-#             stp1 = y[:,:,2:5]
-#             w = tf.constant([1,5,100], dtype=tf.float32)
-#             lPd = tf.math.log(tf.reduce_sum(pi * N(xtp1, mux, sigmax) * N(ytp1, muy, sigmay), axis=-1))
-#             lPs= tf.reduce_sum(w * stp1 * tf.math.log(p), axis=-1)
-#             loss = - (lPd + lPs)
-#
-#         # Compute gradients
-#         trainable_vars = self.trainable_variables
-#         gradients = tape.gradient(loss, trainable_vars)
-#
-#         # Update weights
-#         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-#
-#         # Compute our own metrics
-#         loss_tracker.update_state(loss)
-#       #  mae_metric.update_state(y, y_pred)
-#         return {"loss": loss_tracker.result()}
-#
-#     @property
-#     def metrics(self):
-#         # We list our `Metric` objects here so that `reset_states()` can be
-#         # called automatically at the start of each epoch
-#         # or at the start of `evaluate()`.
-#         # If you don't implement this property, you have to call
-#         # `reset_states()` yourself at the time of your choosing.
-#         return [loss_tracker]
-
-# Construct an instance of CustomModel
-# inputs = keras.Input(shape=(None,6))
-# gru_out = rnn_layer(inputs)
-# outputs = post_process_layer(gru_out)
-# model = CustomModel(inputs, outputs)
-
-# We don't passs a loss or metrics here.
-# model.compile(optimizer="adam")
-#
 # class CustomCallback(keras.callbacks.Callback):
 #     def __init__(self, model):
 #         self.model = model
@@ -258,8 +238,7 @@ model.fit(take_batches, steps_per_epoch=100, epochs=100)
 #     def on_epoch_end(self, epoch):
 #         y_pred = self.model.predict()
 #         print('y predicted: ', y_pred)
-#
-#
-#
-#model.fit(take_batches, steps_per_epoch=100, epochs=100, callbacks=[CustomCallback(model)])
+
+
+
 
